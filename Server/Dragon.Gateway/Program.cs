821b68f9b9f27128e42a3f976f008c77838324e0
lion <xiaolige945@vip.qq.com>
2016-06-25 01:55:26 +0800

    添加Dragon.Gateway工程。

---
+++ Server/Dragon.Gateway/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragon.Gateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
